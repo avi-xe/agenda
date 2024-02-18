32ca14341408a6b0a729218b6b10f538223253a2
avi-xe <Andrea Bellina>
2024-02-18 23:06:08 +0100

    added tag selector in new item

--- js/main.js
+++ js/main.js
@@ -22,6 +22,7 @@ var tags = [];
 
 function onload() {
   setupAddTagsListener();
+  setupRemoveTagsListener();
 }
 
 function setupAddTagsListener() {
@@ -39,9 +40,30 @@ function setupAddTagsListener() {
       tags.push(newTag);
       var elAddItemTags = document.getElementById("add-item-tags");
       if (elAddItemTags != null) {
-        elAddItemTags.replaceWith(createTagsContainer());
+        elAddItemTags.childNodes.forEach((dropdown) => {
+          console.log(dropdown);
+          dropdown.replaceWith(createDropdown());
+        });
       }
     }
+
+    tags.length > 0
+      ? (document.getElementById("remove-tag").disabled = false)
+      : (document.getElementById("remove-tag").disabled = true);
+  });
+}
+
+function setupRemoveTagsListener() {
+  var el = document.getElementById("remove-tag");
+  el.addEventListener("click", function listener() {
+    if (tags.length == 0) {
+      console.log("no tags");
+      return;
+    }
+
+    tags.length > 0
+      ? (document.getElementById("remove-tag").disabled = false)
+      : (document.getElementById("remove-tag").disabled = true);
   });
 }
 
@@ -76,13 +98,51 @@ function createAddItemContainer(elAddItemCard) {
   description.placeholder = "Description";
   description.id = "add-item-container-description";
 
-  var tags = createTagsContainer(elAddItemCard);
+  var elAddItemTagsContainer = document.createElement("div");
+  elAddItemTagsContainer.classList.add("add-item-tags-container");
+  elAddItemTagsContainer.id = "add-item-tags-container";
+  var elAddItemTags = document.createElement("div");
+  elAddItemTags.textContent = "+";
+  var elAddItemChosenTags = document.createElement("div");
+  elAddItemChosenTags.classList.add("add-item-chosen-tags");
+  elAddItemChosenTags.id = "add-item-tags";
+  elAddItemTags.addEventListener("click", () => {
+    if (tags.length > elAddItemChosenTags.childElementCount) {
+      var el = createDropdown();
+      elAddItemChosenTags.appendChild(el);
+    }
+  });
 
   container.appendChild(title);
   container.appendChild(description);
-  container.appendChild(tags);
+  container.appendChild(elAddItemTags);
+  container.appendChild(elAddItemChosenTags);
 
   return container;
+}
+
+function createDropdown() {
+  var el = document.createElement("select");
+  var elAddItemTagsPlaceholder = document.createElement("option");
+  el.classList.add("add-item-tags");
+  el.disabled = true;
+  el.appendChild(elAddItemTagsPlaceholder);
+  if (tags.length > 0) {
+    el.disabled = false;
+    elAddItemTagsPlaceholder.textContent = "--Choose Tag--";
+    elAddItemTagsPlaceholder.disabled = true;
+    elAddItemTagsPlaceholder.selected = true;
+    elAddItemTagsPlaceholder.hidden = true;
+    tags.forEach((tag) => {
+      var option = document.createElement("option");
+      option.textContent = tag;
+      el.options.add(option);
+    });
+  } else {
+    elAddItemTagsPlaceholder.textContent = "Please add a Tag";
+  }
+
+  return el;
 }
 
 function createAddItemButtons(
@@ -91,6 +151,7 @@ function createAddItemButtons(
   elAddItemContainer
 ) {
   var buttons = document.createElement("div");
+  buttons.classList.add("add-item-buttons");
   var ok = document.createElement("div");
   ok.textContent = "OK";
   ok.classList.add("add-item-ok");
@@ -101,12 +162,20 @@ function createAddItemButtons(
     var description = elAddItemContainer.querySelector(
       "#add-item-container-description"
     ).value;
-    addRegularItem(title, description);
+    var tagNodes =
+      elAddItemContainer.querySelector("#add-item-tags").childNodes;
+    var tags = [];
+    tagNodes.forEach((tagNode) => {
+      tags.push(tagNode.options[tagNode.selectedIndex].text);
+    });
+
+    console.log(tags);
+    addRegularItem(title, description, tags);
     elAddItemCard.replaceWith(elAddItemButton);
   });
 
   var ko = document.createElement("div");
-  ko.textContent = "KO";
+  ko.textContent = "Cancel";
   ko.classList.add("add-item-ko");
   ko.addEventListener("click", () => {
     elAddItemCard.replaceWith(elAddItemButton);
@@ -116,31 +185,6 @@ function createAddItemButtons(
   buttons.appendChild(ko);
 
   return buttons;
-}
-
-function createTagsContainer() {
-  var elAddItemTagsContainer = document.createElement("div");
-  elAddItemTagsContainer.classList.add("add-item-tags-container");
-  elAddItemTagsContainer.id = "add-item-tags-container";
-  var elAddItemTags = document.createElement("select");
-  var elAddItemTagsPlaceholder = document.createElement("option");
-  elAddItemTags.id = "add-item-tags";
-  elAddItemTags.disabled = true;
-  elAddItemTags.appendChild(elAddItemTagsPlaceholder);
-  if (tags.length > 0) {
-    elAddItemTags.disabled = false;
-    elAddItemTagsPlaceholder.textContent = "--Choose Tag--";
-    tags.forEach((tag) => {
-      var el = document.createElement("option");
-      el.textContent = tag;
-      elAddItemTags.options.add(el);
-    });
-  } else {
-    elAddItemTagsPlaceholder.textContent = "Please add a Tag";
-  }
-
-  elAddItemTagsContainer.appendChild(elAddItemTags);
-  return elAddItemTagsContainer;
 }
 
 function addTestItem() {
